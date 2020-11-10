8c461f174580fed763f061cb096d8032abc1541c
AaronA35 <aarchamp@terpmail.umd.edu>
2020-11-10 13:24:15 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,96 +1,36 @@
-function convertRestaurantsToCategories(restaurantList) {
-  const newDataShape = restaurantList.reduce((list, item, i) => {
-    const findCat = list.find((findItem) => findItem.label === item.category);
-
-    if (!findCat) {
-      list.push({
-        label: item.category,
-        y: 1
-      });
-    } else {
-      findCat.y += 1;
-    }
-    return list;
-  }, []);
-  return newDataShape;
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const restaurant = [];
+ 
+fetch(endpoint).then(blob => blob.json()).then(data => restaurant.push(...data));
+ 
+function findMatches(wordToMatch, restaurant) {
+   return restaurant.filter(establishments => {
+       const regex = new RegExp(wordToMatch, 'gi');
+       return establishments.name.match(regex) || establishments.category.match(regex)
+   });
 }
-
-function makeYourOptionsObject(datapointsFromRestaurantsList) {
-  // set your chart configuration here
-  CanvasJS.addColorSet('customColorSet1', [
-    // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
-  ]);
-
-  return {
-    animationEnabled: true,
-    colorSet: 'customColorSet1',
-    title: {
-      text: 'Places To Eat Out In Future'
-    },
-    axisX: {
-      interval: 1,
-      labelFontSize: 12
-    },
-    axisY2: {
-      interlacedColor: 'rgba(1,77,101,.2)',
-      gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
-      labelFontSize: 12,
-      scaleBreaks: {customBreaks: [{
-        startValue: 40,
-        endValue: 50,
-        color: 'purple',
-        type: 'straight'
-      },
-      {
-        startValue: 85,
-        endValue: 100,
-        color: 'purple',
-        type: 'straight'
-      },
-      {
-        startValue: 140,
-        endValue: 175,
-        color: 'purple',
-        type: 'straight'
-      }]
-      } // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
-    },
-    data: [{
-      type: 'bar',
-      name: 'restaurants',
-      axisYType: 'secondary',
-      dataPoints: datapointsFromRestaurantsList
-    }]
-  };
+ 
+function displayMatches() {
+   const matchArray = findMatches(this.value, restaurant);
+   const html = matchArray.map(establishments => {
+       return `
+        <li>
+           <span class="name">${establishments.name}</span><br>
+           <span class="category">${establishments.category}</span><br>
+           <span class="address"><address>${establishments.address_line_1}, ${establishments.address_line_2}, ${establishments.city}, ${establishments.state}, ${establishments.zip}
+           </address></span>
+       </li>
+       `;
+   }).join('');
+   suggestions.innerHTML=html;
+  
+   const replaceRegex = new RegExp("------,",'gi');
+   delEmptyAddress = html.replace(replaceRegex," ");
+   suggestions.innerHTML=delEmptyAddress; 
 }
-
-function runThisWithResultsFromServer(jsonFromServer) {
-  console.log('jsonFromServer', jsonFromServer);
-  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
-  // Process your restaurants list
-  // Make a configuration object for your chart
-  // Instantiate your chart
-  const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
-  const options = makeYourOptionsObject(reorganizedData);
-  const chart = new CanvasJS.Chart('chartContainer', options);
-  chart.render();
-}
-
-// Leave lines 52-67 alone; do your work in the functions above
-document.body.addEventListener('submit', async (e) => {
-  e.preventDefault(); // this stops whatever the browser wanted to do itself.
-  const form = $(e.target).serializeArray();
-  fetch('/api', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(form)
-  })
-    .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
-    .catch((err) => {
-      console.log(err);
-    });
-});
+ 
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+ 
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
